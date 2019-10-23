ecdd0272125f10e8ec3783c70181c632a45d70df
heczxysj <842134719@qq.com>
2019-10-24 02:05:43 +0800

    gateway

---
+++ leyou-gateway/src/main/java/com/leyou/gateway/config/LeyouCorsConfiguration.java
@@ -0,0 +1,39 @@
+package com.leyou.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域名访问控制类
+ */
+@Configuration
+public class LeyouCorsConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        //初始化cors配置对象
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //允许跨域的域名，如果要携带cookies,不能写*， *代表所有域名都可以跨域访问
+        corsConfiguration.addAllowedOrigin("http://manage.leyou.com");
+        corsConfiguration.setAllowCredentials(true);  //允许携带cookies
+        /*corsConfiguration.addAllowedMethod("OPTIONS");
+        corsConfiguration.addAllowedMethod("HEAD");
+        corsConfiguration.addAllowedMethod("GET");
+        corsConfiguration.addAllowedMethod("PUT");
+        corsConfiguration.addAllowedMethod("POST");
+        corsConfiguration.addAllowedMethod("DELETE");
+        corsConfiguration.addAllowedMethod("PATCH");*/
+        corsConfiguration.addAllowedMethod("*"); //代表请求所有方法
+        corsConfiguration.addAllowedHeader("*"); //允许携带所有头信息
+
+        //初始化cors配置源对象
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);//拦截一切请求
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
